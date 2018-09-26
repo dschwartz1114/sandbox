cd82b8fa22e2e30c9bccb353748edd0b2a491bdb
dschwartz1114 <dschwartz1114@users.noreply.github.com>
2018-09-26 12:59:16 -0400

    Update and rename index.php to update-stock.php

---
+++ update-stock.php
@@ -0,0 +1,14 @@
+// Clear Stock
+function clear_current_stock(){ 
+// Declare the $wpdb var
+    global $wpdb;
+// Run My SQL Query (My Query is setting the meta value for stock to zero)
+    $wpdb->query("UPDATE wp_postmeta SET meta_value = 0 WHERE meta_key = '_stock'");
+// Check if there were any errors or if my query was successful.
+    if ( is_wp_error( $wpdb ) ) {
+         echo $wpdb->get_error_message();
+    }
+    else {
+         echo 'true';
+    }
+}
