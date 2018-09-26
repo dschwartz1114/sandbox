466362c1edccf650db475814faa67f9e309270c7
dschwartz1114 <dschwartz1114@users.noreply.github.com>
2018-09-26 13:01:53 -0400

    Update update-stock.php

--- update-stock.php
+++ update-stock.php
@@ -5,8 +5,9 @@ function clear_current_stock(){
 // Declare the $wpdb var
     global $wpdb;
 // Run My SQL Query (My Query is setting the meta value for stock to zero)
-    $wpdb->query("UPDATE wp_postmeta SET meta_value = 0 WHERE meta_key = '_stock'");
-// Check if there were any errors or if my query was successful.
+// Future will loop in products and update the stock based on imported values
+     $wpdb->query("UPDATE wp_postmeta SET meta_value = 0 WHERE meta_key = '_stock'");
+// Check if there were any errors or if query was successful.
     if ( is_wp_error( $wpdb ) ) {
          echo $wpdb->get_error_message();
     }
